ed0baa6a0bbdbed7b496ab7922dfa358dd7e4a4c
sangjuneeeee <wns1169@gmail.com>
2024-06-11 19:24:54 +0900

    hotfix

--- scripts/canvas.js
+++ scripts/canvas.js
@@ -30,186 +30,193 @@ const MODE_BUTTON = [brush, erase];
 let mode = brush;
 let painting = false;
 
+const words = [
+	"사과",
+	"고양이",
+	"노트북",
+	"물고기",
+	"바닷가",
+	"요정",
+	"새",
+	"나무",
+	"호숫가",
+	"밤",
+]; // 미리 저장한 제시어
 
-const words = ["사과", "고양이", "노트북", "물고기", "바닷가", "요정", "새", "나무", "호숫가", "밤"]; // 미리 저장한 제시어
-
-function startPainting() { painting = true; }
-function stopPainting() { painting = false; }
+function startPainting() {
+	painting = true;
+}
+function stopPainting() {
+	painting = false;
+}
 
 function onMouseMove(event) {
-    const x = event.offsetX;
-    const y = event.offsetY;
-    if (mode === brush) {
-        if (!painting) {
-            ctx.beginPath();
-            ctx.moveTo(x, y);
-        }
-        else {
-            ctx.lineTo(x, y);
-            ctx.stroke();
-        }
-    }
-    else if (mode === erase) {
-        if (painting) {
-            ctx.clearRect(x - ctx.lineWidth / 2, y - ctx.lineWidth / 2, ctx.lineWidth, ctx.lineWidth);
-        }
-    }
+	const x = event.offsetX;
+	const y = event.offsetY;
+	if (mode === brush) {
+		if (!painting) {
+			ctx.beginPath();
+			ctx.moveTo(x, y);
+		} else {
+			ctx.lineTo(x, y);
+			ctx.stroke();
+		}
+	} else if (mode === erase) {
+		if (painting) {
+			ctx.clearRect(x - ctx.lineWidth / 2, y - ctx.lineWidth / 2, ctx.lineWidth, ctx.lineWidth);
+		}
+	}
 }
-
 
 function handleModeChange(event) {
-    mode = event.target;
-    for (let i = 0; i < MODE_BUTTON.length; i++) {
-        const button = MODE_BUTTON[i];
-        if (button === mode) {
-            button.style.backgroundColor = "skyblue";
-        }
-        else {
-            button.style.backgroundColor = "white";
-        }
-    }
-    if (mode === brush) {
-        ctx.strokeStyle = INITIAL_COLOR;
-    }
+	mode = event.target;
+	for (let i = 0; i < MODE_BUTTON.length; i++) {
+		const button = MODE_BUTTON[i];
+		if (button === mode) {
+			button.style.backgroundColor = "skyblue";
+		} else {
+			button.style.backgroundColor = "white";
+		}
+	}
+	if (mode === brush) {
+		ctx.strokeStyle = INITIAL_COLOR;
+	}
 }
-
 
 function startTimer() {
-    const timerDuration = 20000; // 30초
-    let timeLeft = timerDuration / 1000;
+	const timerDuration = 20000; // 30초
+	let timeLeft = timerDuration / 1000;
 
-    // 제시어 선택
-    const randomIndex = Math.floor(Math.random() * words.length);
-    const selectedWord = words[randomIndex];
-    wordDisplay.textContent = `${selectedWord}`;
+	// 제시어 선택
+	const randomIndex = Math.floor(Math.random() * words.length);
+	const selectedWord = words[randomIndex];
+	wordDisplay.textContent = `${selectedWord}`;
 
-    const minutes = Math.floor(timeLeft / 60);
-    const seconds = Math.floor(timeLeft % 60);
-    timerDisplay.textContent = `${minutes.toString().padStart(2, '0')}:${seconds.toString().padStart(2, '0')}`;
+	const minutes = Math.floor(timeLeft / 60);
+	const seconds = Math.floor(timeLeft % 60);
+	timerDisplay.textContent = `${minutes.toString().padStart(2, "0")}:${seconds
+		.toString()
+		.padStart(2, "0")}`;
 
-    timerInterval = setInterval(() => {
-        timeLeft--;
+	timerInterval = setInterval(() => {
+		timeLeft--;
 
-        const minutes = Math.floor(timeLeft / 60);
-        const seconds = Math.floor(timeLeft % 60);
-        timerDisplay.textContent = `${minutes.toString().padStart(2, '0')}:${seconds.toString().padStart(2, '0')}`;
+		const minutes = Math.floor(timeLeft / 60);
+		const seconds = Math.floor(timeLeft % 60);
+		timerDisplay.textContent = `${minutes.toString().padStart(2, "0")}:${seconds
+			.toString()
+			.padStart(2, "0")}`;
 
-        if (timeLeft <= 0) {
-            clearInterval(timerInterval);
-            alert("시간 종료");
-            saveDrawing(selectedWord);
-        }
-    }, 1000);
+		if (timeLeft <= 0) {
+			clearInterval(timerInterval);
+			alert("시간 종료");
+			saveDrawing(selectedWord);
+		}
+	}, 1000);
 }
 
-
 function handleRangeChange(event) {
-    const size = event.target.value;
-    ctx.lineWidth = size;
-    range.value = size;
+	const size = event.target.value;
+	ctx.lineWidth = size;
+	range.value = size;
 }
 
 function onLineWidthChange(event) {
-    ctx.lineWidth = event.target.value;
+	ctx.lineWidth = event.target.value;
 }
 
 function onColorChange(event) {
-    ctx.strokeStyle = event.target.value;
-    ctx.fillStyle = event.target.value;
+	ctx.strokeStyle = event.target.value;
+	ctx.fillStyle = event.target.value;
 }
-
 
 function onColorClik(event) {
-    const colorValue = event.target.dataset.color;
-    ctx.strokeStyle = colorValue;
-    ctx.fillStyle = colorValue;
-    color.value = colorValue;
+	const colorValue = event.target.dataset.color;
+	ctx.strokeStyle = colorValue;
+	ctx.fillStyle = colorValue;
+	color.value = colorValue;
 }
-
 
 function clearCanvas() {
-    ctx.clearRect(0, 0, CANVAS_WIDTH_SIZE, CANVAS_HEIGHT_SIZE);
+	ctx.clearRect(0, 0, CANVAS_WIDTH_SIZE, CANVAS_HEIGHT_SIZE);
 }
-
 
 function saveDrawing(word) {
-    let drawings = JSON.parse(localStorage.getItem("drawings")) || [];
-    const existingDrawings = drawings.filter(d => d.title.startsWith(word));
-    const count = existingDrawings.length;
-    const title = `${word}`;
-    const dataURL = canvas.toDataURL();
+	let drawings = JSON.parse(localStorage.getItem("drawings")) || [];
+	const existingDrawings = drawings.filter((d) => d.title.startsWith(word));
+	const count = existingDrawings.length;
+	const title = `${word}_${count + 1}`;
+	const dataURL = canvas.toDataURL();
 
-    drawings.push({ title, dataURL });
-    localStorage.setItem("drawings", JSON.stringify(drawings));
-    alert("그림이 저장되었습니다.");
+	drawings.push({ title, dataURL });
+	localStorage.setItem("drawings", JSON.stringify(drawings));
+	alert("그림이 저장되었습니다.");
 }
 
-
 function loadCanvas() {
-    const drawings = JSON.parse(localStorage.getItem("drawings")) || [];
-    const titles = drawings.map(d => d.title).join("\n");
-    const title = prompt(`불러올 그림의 제목을 입력하세요:\n${titles}`);
-    const drawing = drawings.find(d => d.title === title);
-    if (drawing) {
-        const img = new Image();
-        img.src = drawing.dataURL;
-        img.onload = function () {
-            const CANVAS_SIZE_WIDTH = canvas.width;
-            const CANVAS_SIZE_HEIGHT = canvas.height;
-            ctx.clearRect(0, 0, CANVAS_SIZE_WIDTH, CANVAS_SIZE_HEIGHT);
-            ctx.drawImage(img, 0, 0, CANVAS_SIZE_WIDTH, CANVAS_SIZE_HEIGHT);
-        }
-    } else {
-        alert("그림을 찾을 수 없습니다.");
-    }
+	const drawings = JSON.parse(localStorage.getItem("drawings")) || [];
+	const titles = drawings.map((d) => d.title).join("\n");
+	const title = prompt(`불러올 그림의 제목을 입력하세요:\n${titles}`);
+	const drawing = drawings.find((d) => d.title === title);
+	if (drawing) {
+		const img = new Image();
+		img.src = drawing.dataURL;
+		img.onload = function () {
+			const CANVAS_SIZE_WIDTH = canvas.width;
+			const CANVAS_SIZE_HEIGHT = canvas.height;
+			ctx.clearRect(0, 0, CANVAS_SIZE_WIDTH, CANVAS_SIZE_HEIGHT);
+			ctx.drawImage(img, 0, 0, CANVAS_SIZE_WIDTH, CANVAS_SIZE_HEIGHT);
+		};
+	} else {
+		alert("그림을 찾을 수 없습니다.");
+	}
 }
 
 function deleteCanvas() {
-    const drawings = JSON.parse(localStorage.getItem("drawings")) || [];
-    const titles = drawings.map(d => d.title).join("\n");
-    const title = prompt(`삭제할 그림의 제목을 입력하세요:\n${titles}`);
-    const newDrawings = drawings.filter(d => d.title !== title);
+	const drawings = JSON.parse(localStorage.getItem("drawings")) || [];
+	const titles = drawings.map((d) => d.title).join("\n");
+	const title = prompt(`삭제할 그림의 제목을 입력하세요:\n${titles}`);
+	const newDrawings = drawings.filter((d) => d.title !== title);
 
-    if (newDrawings.length < drawings.length) {
-        localStorage.setItem("drawings", JSON.stringify(newDrawings));
-        alert("그림이 삭제되었습니다.");
-    }
+	if (newDrawings.length < drawings.length) {
+		localStorage.setItem("drawings", JSON.stringify(newDrawings));
+		alert("그림이 삭제되었습니다.");
+	}
 }
 
 var animateButton = function (e) {
-    e.preventDefault;
-    e.target.classList.remove('animate');
-    e.target.classList.add('animate');
-    setTimeout(function () {
-        e.target.classList.remove('animate');
-    }, 700);
+	e.preventDefault;
+	e.target.classList.remove("animate");
+	e.target.classList.add("animate");
+	setTimeout(function () {
+		e.target.classList.remove("animate");
+	}, 700);
 };
 
 var bubblyButtons = document.getElementsByClassName("bubbly-button");
 
 for (var i = 0; i < bubblyButtons.length; i++) {
-    bubblyButtons[i].addEventListener('click', animateButton, false);
+	bubblyButtons[i].addEventListener("click", animateButton, false);
 }
 
 function exitPage() {
-    window.location.href = "../index.html#cards-container"
+	window.location.href = "../index.html#cards-container";
 }
 
 if (canvas) {
-    canvas.addEventListener("mousemove", onMouseMove);
-    canvas.addEventListener("mousedown", startPainting);
-    canvas.addEventListener("mouseup", stopPainting);
-    canvas.addEventListener("mouseleave", stopPainting);
+	canvas.addEventListener("mousemove", onMouseMove);
+	canvas.addEventListener("mousedown", startPainting);
+	canvas.addEventListener("mouseup", stopPainting);
+	canvas.addEventListener("mouseleave", stopPainting);
 }
 
-MODE_BUTTON.forEach(mode => mode.addEventListener("click", handleModeChange));
+MODE_BUTTON.forEach((mode) => mode.addEventListener("click", handleModeChange));
 startTimerBtn.addEventListener("click", startTimer);
 range.addEventListener("input", onLineWidthChange);
 color.addEventListener("change", onColorChange);
-colorOptions.forEach(color => color.addEventListener("click", onColorClik));
+colorOptions.forEach((color) => color.addEventListener("click", onColorClik));
 clearCanvasBtn.addEventListener("click", clearCanvas);
 
 loadCanvasBtn.addEventListener("click", loadCanvas);
 deleteCanvasBtn.addEventListener("click", deleteCanvas);
 exitPageBtn.addEventListener("click", exitPage);
-
